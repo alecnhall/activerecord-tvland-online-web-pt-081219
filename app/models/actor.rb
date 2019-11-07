e4acca3c94c5c708e51806286d5d34dd036ab38b
Alec Hall <alecnhall@gmail.com>
2019-11-07 15:50:56 -0700

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+    belongs_to :show
+    has_many :characters
+
+    def full_name 
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        Actor.all.map do |a|
+        a.characters.map do |c|
+            "#{c.name} - #{c.show.name}"
+            end
+        end.flatten
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list 
+    Show.all.map do |s|
+        s.actors.map do |a|
+          "#{a.first_name} #{a.last_name}"
+        end
+    end.flatten  
+  end
 end
